1b6e7f4947c24cef8409f81ddde2435060eaf844
LiesnerDev <liesnerdev33@gmail.com>
2025-09-24 09:47:31 -0300

    Modernization Code: test hotfix 1079, File: consolidated-frontend.zip

---
+++ src/components/FormList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="form-list">
+    <h2>Lista de Formulários</h2>
+    <ul>
+      <li 
+        v-for="form in forms" 
+        :key="form.Id"
+        @click="selectForm(form)"
+      >
+        {{ form.Name }} - {{ form.FormType }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FormList',
+  props: {
+    forms: {
+      type: Array,
+      required: true
+    }
+  },
+  methods: {
+    selectForm(form) {
+      this.$emit('formSelected', form);
+    }
+  }
+};
+</script>
+
+<style src="./FormList.css" scoped></style>
